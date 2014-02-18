1d81c4e71bb7d6b520d2e420b23b92654179377c
jardix22 <jardix22@gmail.com>
2014-02-18 15:22:33 -0500

    database created

---
+++ src/Taller/BookBundle/BookBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Taller\BookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BookBundle extends Bundle
+{
+}
---
+++ src/Taller/BookBundle/Entity/Book.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Taller\BookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Book
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=200)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=200)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="summary", type="text")
+     */
+    private $summary;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="edition_date", type="date")
+     */
+    private $editionDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     * @return Book
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string 
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set editionDate
+     *
+     * @param \DateTime $editionDate
+     * @return Book
+     */
+    public function setEditionDate($editionDate)
+    {
+        $this->editionDate = $editionDate;
+
+        return $this;
+    }
+
+    /**
+     * Get editionDate
+     *
+     * @return \DateTime 
+     */
+    public function getEditionDate()
+    {
+        return $this->editionDate;
+    }
+}
